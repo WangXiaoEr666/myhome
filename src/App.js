8a6f7b10fdba80db8e307de0ef0ac19e27dde909
wangtingting <1328483147@qq.com>
2019-03-08 17:17:03 +0800

    完成首页路由配置

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './App.css'
+// 导入登录页面样式
+import './Login.css'
+// 导入登录页面组件
+import Login from './Login'
+
+// 导入主页面
+import Main from './module/main'
+
+
 
 class App extends Component {
   render() {
-    return <div>主页</div>
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Login} />
+          <Route path="/home" component={Main} />
+        </Switch>
+      </BrowserRouter>
+    )
   }
 }
 
